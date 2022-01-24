819955eed457774446d4ca818b80c2fd4963d887
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-24 18:25:04 -0500

    Added code to use built-in handlebar functions

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
