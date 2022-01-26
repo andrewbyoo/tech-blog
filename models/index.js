cfed8feae48d84d2db26b1ca14be6a2666a306d2
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-25 22:17:53 -0500

    Removed comments no longer needed

--- models/index.js
+++ models/index.js
@@ -2,13 +2,11 @@ const User = require('./User');
 const Post = require('./Post');
 
 User.hasMany(Post, {
-  // Add foreign key to post primary key once created
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
 Post.belongsTo(User, {
-  // Add foreign key to user
   foreignKey: 'user_id'
 })
 
