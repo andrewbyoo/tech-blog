217abe16288a46f3e2b28a7d6f25a51d658ff3f0
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-25 22:48:43 -0500

    Added models and added rendering of post data on homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,17 @@
 const router = require('express').Router();
+const { User, Post } = require('../models');
 
 router.get('/', async (req, res) => {
-  res.render('homepage');
+  try {
+    const dbPostData = await Post.findAll({ include: [{ model: User }] });
+    const posts = dbPostData.map(post => post.get({ plain: true }));
+
+    console.log(posts)
+    res.render('homepage', {posts});
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
