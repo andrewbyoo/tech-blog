aa5aff58454a2b11e015f6a82bf8ab01c33800d6
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-26 00:12:45 -0500

    Added logged in key

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,7 +5,7 @@ router.get('/', async (req, res) => {
   try {
     const dbPostData = await Post.findAll({ include: [{ model: User }] });
     const posts = dbPostData.map(post => post.get({ plain: true }));
-    res.render('homepage', { posts });
+    res.render('homepage', { posts, loggedIn: req.session.loggedIn });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
