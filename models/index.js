d9eeb488ea77dd382107186f7da8f6a4b2e33525
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-25 21:49:40 -0500

    Added foreign keys

--- models/index.js
+++ models/index.js
@@ -3,10 +3,13 @@ const Post = require('./Post');
 
 User.hasMany(Post, {
   // Add foreign key to post primary key once created
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
 });
 
 Post.belongsTo(User, {
   // Add foreign key to user
+  foreignKey: 'user_id'
 })
 
 module.exports = { User };
