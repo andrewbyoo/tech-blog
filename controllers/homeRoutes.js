fd3ca2ce0430b4bf4aaf21112f17986849181e1f
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-25 23:01:43 -0500

    Remove console.log

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,9 +5,7 @@ router.get('/', async (req, res) => {
   try {
     const dbPostData = await Post.findAll({ include: [{ model: User }] });
     const posts = dbPostData.map(post => post.get({ plain: true }));
-
-    console.log(posts)
-    res.render('homepage', {posts});
+    res.render('homepage', { posts });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
