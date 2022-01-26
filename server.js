bd06db94df0328e4ca67c21a4038cedfd4842c22
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-25 23:29:23 -0500

    Added connect-session-sequelize and express-session node modules

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 const path = require('path');
 const express = require('express');
+var session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Set up sessions with cookies
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    // Stored in milliseconds (86400 === 1 day)
+    maxAge: 86400,
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
 
 const hbs = exphbs.create({ helpers });
 
