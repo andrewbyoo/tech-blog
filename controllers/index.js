824c0d1aef977525cfea87596bf38960eb776888
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-25 23:05:17 -0500

    Added dashboard route

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const homeRoutes = require('./homeRoutes');
+const dashboardRoutes = require('./dashboardRoutes');
 
 router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 module.exports = router;
