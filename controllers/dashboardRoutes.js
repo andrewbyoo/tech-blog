d798bae6fd1438df2e3f534aff2224e4276c5de1
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-26 01:47:50 -0500

    Added route for rendering the dashboard

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,4 +1,15 @@
 const router = require('express').Router();
 const { User, Post } = require('../models');
 
+router.get('/', async (req, res) => {
+  try {
+    const dbPostData = await Post.findAll({ include: [{ model: User }], where: User.id = req.session.userId });
+    const posts = dbPostData.map(post => post.get({ plain: true }));
+    res.render('dashboard', { posts, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  };
+});
+
 module.exports = router;
