766ab76c1cce7459e8d22e8fa7959bd5f5395450
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-25 23:43:35 -0500

    Added login route

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 const homeRoutes = require('./homeRoutes');
 const dashboardRoutes = require('./dashboardRoutes');
+const loginRoutes = require('./loginRoutes');
 const apiRoutes = require('./api');
 
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
+router.use('/login', loginRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
