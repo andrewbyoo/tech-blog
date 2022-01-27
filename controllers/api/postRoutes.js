719a3221d122496291819171f805c03c4193724c
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-26 21:59:50 -0500

    Added get route for all posts

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,3 +1,17 @@
 const router = require('express').Router();
 const { Post, User } = require('../../models');
 const withAuth = require('../../utils/auth');
+
+router.get('/', async (req, res) => {
+  try {
+    const dbPostData = await Post.findAll({ include: [{ model: User, attributes: { exclude: 'password' } }] });
+    const posts = dbPostData.map(post => post.get({ plain: true }));
+    console.log(posts)
+    res.status(200).json(posts)
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  };
+});
+
+module.exports = router;
