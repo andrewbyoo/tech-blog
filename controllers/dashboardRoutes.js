f6653713ab787e501175df17d2f83d82a371f167
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-27 09:59:59 -0500

    Changed finds to not include the user as it is not necessary

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -4,7 +4,7 @@ const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, async (req, res) => {
   try {
-    const dbPostData = await Post.findAll({ include: [{ model: User, attributes: { exclude: 'password' } }], where: User.id = req.session.userId });
+    const dbPostData = await Post.findAll({ where: { user_id: req.session.userId } });
     const posts = dbPostData.map(post => post.get({ plain: true }));
     res.render('dashboard', { posts, loggedIn: req.session.loggedIn });
   } catch (err) {
@@ -15,9 +15,8 @@ router.get('/', withAuth, async (req, res) => {
 
 router.get('/post/:id', withAuth, async (req, res) => {
   try {
-    const dbPostData = await Post.findByPk(req.params.id, { include: [{ model: User, attributes: { exclude: ['password'] } }], where: User.id = req.session.userId });
+    const dbPostData = await Post.findByPk(req.params.id, { where: { user_id: req.session.userId } });
     const post = dbPostData.get({ plain: true });
-    console.log(post)
     res.render('user-post', { ...post, loggedIn: req.session.loggedIn });
   } catch (err) {
     console.log(err);
@@ -27,9 +26,8 @@ router.get('/post/:id', withAuth, async (req, res) => {
 
 router.get('/post/:id/edit', withAuth, async (req, res) => {
   try {
-    const dbPostData = await Post.findByPk(req.params.id, { include: [{ model: User, attributes: { exclude: ['password'] } }], where: User.id = req.session.userId });
+    const dbPostData = await Post.findByPk(req.params.id, { where: { user_id: req.session.userId } });
     const post = dbPostData.get({ plain: true });
-    console.log(post)
     res.render('edit-post', { ...post, loggedIn: req.session.loggedIn })
   } catch (err) {
     console.log(err);
