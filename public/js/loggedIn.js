1bc4525ad6926c59f36e6e72afdc990741734654
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-27 00:06:32 -0500

    Added PUT method (currently not working)

--- public/js/loggedIn.js
+++ public/js/loggedIn.js
@@ -13,3 +13,28 @@ const logout = async () => {
 };
 
 document.querySelector('#logout').addEventListener('click', logout);
+
+// Edit post
+const updateFormHandler = async (event) => {
+  event.preventDefault();
+
+  const id = document.querySelector('#post-id').value;
+  const editedContent = document.querySelector('#post-edit').value.trim();
+
+  const response = await fetch(`/api/posts/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({ post_content: editedContent }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  if (response.ok) {
+    document.location.replace(`/dashboard/post/${id}`);
+  } else {
+    alert('Could not update post.');
+  }
+
+};
+
+document
+  .querySelector('.edit-form')
+  .addEventListener('submit', updateFormHandler);
