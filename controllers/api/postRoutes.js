2641cd5825475846854c3e80380530505547c385
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-27 19:41:09 -0500

    Added comments model

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    const dbPostData = await Post.findAll({ include: [{ model: User, attributes: { exclude: 'password' } }] });
+    const dbPostData = await Post.findAll({ include: [{ model: User, attributes: { exclude: 'password' } }, { model: Comment }] });
     const posts = dbPostData.map(post => post.get({ plain: true }));
     console.log(posts)
     res.status(200).json(posts)
@@ -31,8 +31,9 @@ router.post('/', withAuth, async (req, res) => {
 
 router.get('/:id', async (req, res) => {
   try {
-    const dbPostData = await Post.findByPk(req.params.id, { include: [{ model: User, attributes: { exclude: 'password' } }] });
+    const dbPostData = await Post.findByPk(req.params.id, { include: [{ model: User, attributes: { exclude: 'password' } }, { model: Comment }] });
     const post = dbPostData.get({ plain: true });
+    console.log(post)
     res.status(200).json(post)
   } catch (err) {
     console.log(err);
